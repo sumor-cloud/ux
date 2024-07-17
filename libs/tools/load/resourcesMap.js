166b7b543fca6b97b87fe22d5f7e55ddb1d19381
ohnow <dennis@sumor.com>
2024-07-17 09:47:36 +0000

    change cdnjs

--- libs/tools/load/resourcesMap.js
+++ libs/tools/load/resourcesMap.js
@@ -1,21 +1,21 @@
 export default {
   vconsole: {
-    js: 'https://cdn.bootcdn.net/ajax/libs/vConsole/3.15.1/vconsole.min.js'
+    js: 'https://cdnjs.cloudflare.com//ajax/libs/vConsole/3.15.1/vconsole.min.js'
   },
   qrcode: {
-    js: 'https://cdn.bootcdn.net/ajax/libs/qrcode/1.5.1/qrcode.min.js'
+    js: 'https://cdnjs.cloudflare.com//ajax/libs/qrcode/1.5.1/qrcode.min.js'
   },
   xlsx: {
-    js: 'https://cdn.bootcdn.net/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+    js: 'https://cdnjs.cloudflare.com//ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
   },
   dayjs: {
-    js: 'https://cdn.bootcdn.net/ajax/libs/dayjs/1.10.8/dayjs.min.js',
-    prefix: 'https://cdn.bootcdn.net/ajax/libs/dayjs/1.10.8'
+    js: 'https://cdnjs.cloudflare.com//ajax/libs/dayjs/1.10.8/dayjs.min.js',
+    prefix: 'https://cdnjs.cloudflare.com//ajax/libs/dayjs/1.10.8'
   },
   jszip: {
-    js: 'https://cdn.bootcdn.net/ajax/libs/jszip/3.10.1/jszip.min.js'
+    js: 'https://cdnjs.cloudflare.com//ajax/libs/jszip/3.10.1/jszip.min.js'
   },
   highlight: {
-    js: 'https://cdn.bootcdn.net/ajax/libs/highlight.js/11.9.0/highlight.min.js'
+    js: 'https://cdnjs.cloudflare.com//ajax/libs/highlight.js/11.9.0/highlight.min.js'
   }
 }
